ab08176653440db3bbf0c39c32a4f3faff2c6503
sw1104 <sangw1104@gmail.com>
2024-07-09 14:00:10 +0900

    add llm.py to handle llm

--- chat.py
+++ chat.py
@@ -1,12 +1,6 @@
 import streamlit as st
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import RetrievalQA
-from langchain import hub
-from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from langchain_openai import OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
+from llm import get_ai_message
 
 st.set_page_config(page_title="소득세 챗봇", page_icon="⁉️")
 
@@ -23,35 +17,6 @@ print(f"before : {st.session_state.message_list}")
 for message in st.session_state.message_list:
     with st.chat_message(message["role"]):
         st.write(message["content"])
-
-
-def get_ai_message(user_message):
-    embedding = OpenAIEmbeddings(model='text-embedding-3-large')
-    index_name = 'tax-markdown-index'
-    database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
-
-    llm = ChatOpenAI(model='gpt-4o')
-    prompt = hub.pull('rlm/rag-prompt')
-    retriever = database.as_retriever(search_kwargs={'k': 4})
-
-    qa_chain = RetrievalQA.from_chain_type(llm, retriever=retriever, chain_type_kwargs={'prompt':prompt})
-
-    dictionary = ["사람을 나타내는 표현 => 거주자"]
-
-    prompt = ChatPromptTemplate.from_template(f"""
-        사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
-        만약 변경할 필요가 없다고 판단되면, 사용자의 질문을 변경하지 않아도 됩니다.
-        그런 경우에는 질문만 리턴하세요.
-        사전: {dictionary}
-
-        질문: {{question}}
-    """)
-
-    dictionary_chain = prompt | llm | StrOutputParser()
-    tax_chain = {"query": dictionary_chain} | qa_chain
-    ai_message = tax_chain.invoke({'question': user_message})
-    return ai_message['result']
-
 
 if user_question := st.chat_input(placeholder="소득세에 관련된 궁금한 내용들을 입력하세요."):
     with st.chat_message("user"):
